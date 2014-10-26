c115de0d43d4be5a146ef9f5f8468e430e6f3b3c
ppati000 <ppati000@me.com>
2014-10-26 11:22:15 +0100

    Add distance to MoveRanking factors Add comments to constants

--- src/sc/player2015/logic/MoveRanking.java
+++ src/sc/player2015/logic/MoveRanking.java
@@ -21,10 +21,39 @@ import sc.plugin2015.SetMove;
  * @since 10/21/2014
  */
 public class MoveRanking {
-
+    
+    //CONSTANTS
+    //positive factors
+    /**
+     * Number of credits added to the ranking for each fish on the destination field.
+     * e.g. 2 fish and multiplier = 100 -> 200 credits
+     */
     private final int FISH_MULTIPLIER = 100;
+    /**
+     * Number of credits added for the average number of fish on the adjacent fields.
+     * e.g. 2.3 fish on average and SURROUNDING_FISH_MULTIPLIER = 100 -> 230 credits
+     */
     private final int SURROUNDING_FISH_MULTIPLIER = 100;
-
+    
+    //negative factors
+    /**
+     * Number of credits subtracted from the ranking based on distance between start
+     * and destination fields.
+     * e.g. distance = 4, multiplier = 50 -> -200 credits
+     */
+    private final int DISTANCE_NORMAL_MULTIPLIER = 50;
+    
+    /**
+     * Same as above, but for the last rounds of the game.
+     */
+    private final int DISTANCE_END_MULTIPLIER = 20;
+    
+    /**
+     * Until which round we should use the normal distance multiplier.
+     */
+    private final int DISTANCE_NORMAL_UNTIL_ROUND = 25;
+    
+    
     List<Move> possibleMoves;
     GameState gameState;
     int[] rankings;
@@ -44,8 +73,15 @@ public class MoveRanking {
 
                 ArrayList<int[]> surroundingCoords = CoordCalc.getSurroundingCoordinates(coordinatesOfMove);
                 for (int[] surroundingCoord : surroundingCoords) {
-                    rankings[i] += (SURROUNDING_FISH_MULTIPLIER * gameState.getBoard()
+                    rankings[i] = rankings[i] + (SURROUNDING_FISH_MULTIPLIER * gameState.getBoard()
                             .getFishNumber(surroundingCoord[0], surroundingCoord[1])) / surroundingCoords.size();
+                }
+                
+                if (gameState.getRound() <= DISTANCE_NORMAL_UNTIL_ROUND){
+                    rankings [i] = (int) (rankings[i] - DISTANCE_NORMAL_MULTIPLIER * CoordCalc.distance(possibleMove.getRunCoordinates()));
+                }
+                else {
+                    rankings [i] = (int) (rankings[i] - DISTANCE_END_MULTIPLIER * CoordCalc.distance(possibleMove.getRunCoordinates()));
                 }
             }
             else {
